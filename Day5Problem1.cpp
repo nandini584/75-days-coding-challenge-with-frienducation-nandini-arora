2c05526bb5743e47bc1bb3d3a1b91586f1a60feb
Nandini Arora <nandini.arora.che22@itbhu.ac.in>
2024-01-10 17:46:55 +0530

    problem 1 added

---
+++ Day5Problem1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int p1=1;
+        int p2=1;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                p2*=nums[i];
+            }else{
+                count++;
+            }
+            p1*=nums[i];
+        }
+        for(int i=0;i<nums.size();i++){
+            if(count==1){
+                if(nums[i]==0){
+                    nums[i]=p2;
+                }
+                else{
+                    nums[i]=p1/nums[i];
+                }
+            }else{
+                if(nums[i]==0){
+                nums[i]=p1;
+                }else{
+                    nums[i]=p1/nums[i];
+                }
+            }
+        }
+        return nums;
+    }
+};
